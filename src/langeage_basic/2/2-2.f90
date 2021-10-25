57606c3685debd29b9410274c7bf3f931238f59f
Sumbrella Wang <493972246@qq.com>
2021-10-25 14:30:36 +0800

    增加第二章代码

---
+++ src/langeage_basic/2/2-2.f90
@@ -0,0 +1,9 @@
+!> program 2-2
+program real_demo
+    implicit none
+    real :: a = 1, b = 2
+    integer :: c = 1, d = 2
+
+    write(*, *) a / b
+    write(*, *) c / d
+end program
---
+++ src/langeage_basic/2/2-3.f90
@@ -0,0 +1,7 @@
+!> program 2-3
+program complex_demo
+    implicit none
+    complex :: a = (2., 4.)
+    complex :: b = (3., 5.)
+    write(*, *) a * b
+end program
